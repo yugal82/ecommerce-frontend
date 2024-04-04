f7674b9d1633eb4524af920c2774337fb87b1581
Yugal Khanter <yugalkhanter82@gmail.com>
2024-04-04 13:15:28 +0530

    ecommerce front end setup

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,9 @@
-// A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
-}
+const fetchCount = (amount = 1) => {
+  return new Promise(async (resolve) => {
+    const response = await fetch('http://localhost:8080');
+    const result = await response.json();
+    resolve({ result });
+  });
+};
+
+export { fetchCount };
