4143623ffa77b7b0e79d44106f38be170bcd66b0
Yugal Khanter <yugalkhanter82@gmail.com>
2024-04-04 15:22:14 +0530

    basic home page layout ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Counter from './features/counter/Counter';
+import ProductList from './features/product-list/ProductList';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Navbar />
+      <ProductList />
     </div>
   );
 }
