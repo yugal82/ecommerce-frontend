d805b131405e8e687381360a68ccaf3a21c9e160
Yugal Khanter <yugalkhanter82@gmail.com>
2024-04-30 18:50:50 +0530

    checkedUserAuth code removed temporarily from index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider, useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { selectUserAuthChecked } from './features/auth/authSlice';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const userAuthChecked = useSelector(selectUserAuthChecked);
 
 root.render(
   <React.StrictMode>
-    {userAuthChecked && (
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    )}
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
