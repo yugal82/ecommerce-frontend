e9091527b9c9c23900824e2afd54074cccf7541e
Yugal Khanter <yugalkhanter82@gmail.com>
2024-05-30 13:32:06 +0530

    protected componenet minor change

--- src/components/auth/Protected.js
+++ src/components/auth/Protected.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { selectUserInfo } from '../../features/user/userSlice';
 import { ToastContainer, toast } from 'react-toastify';
+import { selectLoggedInUser } from '../../features/auth/authSlice';
 
 const Protected = ({ children }) => {
-  const user = useSelector(selectUserInfo);
+  const user = useSelector(selectLoggedInUser);
 
   useEffect(() => {
     if (!user) toast.error('Please login', { position: 'bottom-right', autoClose: true, delay: 3000 });
